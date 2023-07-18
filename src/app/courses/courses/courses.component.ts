00b77542463a0966b818b4fd65f4f46d42639f31
Tasso-chaves <tassochav@gmail.com>
2023-07-18 17:54:01 -0300

    Adiciona botões de ação, e formulario de curso

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { CoursesService } from '../services/courses.service';
 import { Observable, catchError, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -13,10 +14,14 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 export class CoursesComponent implements OnInit{
 
   courses$: Observable<Course[]>;
-  displayedColumns: string[] = ['name', 'category'];
+  displayedColumns: string[] = ['name', 'category', 'actions'];
   //dataSource = this.courses;
 
-  constructor(private coursesService: CoursesService, public dialog: MatDialog){
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private  router: Router,
+    private activeRouter: ActivatedRoute){
 
     this.courses$ = this.coursesService.list().pipe(
       catchError(error =>{
@@ -34,5 +39,8 @@ export class CoursesComponent implements OnInit{
 
   }
 
-
+  onAdd(){
+    console.log('onAdd');
+    this.router.navigate(['new'], {relativeTo: this.activeRouter});
+  }
 }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -5,7 +5,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   exports: [
@@ -15,7 +18,9 @@ import {MatButtonModule} from '@angular/material/button';
     MatIconModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
 })
 export class AppMaterialModule { }
